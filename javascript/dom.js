e7f83f91bd6dfefa3223f591b5cf6e12774e082a
Gennadiy <genaydzhan70@gmail.com>
2019-07-12 14:46:20 +0600

    Create renderGame function

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,10 +1,21 @@
-const DOMStuff = (function() {
+const TicTacGUI = (function() {
   const game = TicTacGame(Player("x", "user"), Player("0", "user"));
   const cellsContainer = document.querySelector(".cells");
+  const cells = document.querySelectorAll(".cell")
+  renderGame();
+
   cellsContainer.addEventListener("click", (e) => {
     coords = e.target.id.split("").map((n) => Number(n)); // => [int, int]
     game.move(coords);
+    console.log(coords);
+    renderGame();
   })
 
-
+  function renderGame() {
+    cells.forEach((cell) => {
+      coords = cell.id.split("").map((n) => Number(n));
+      symbol = Gameboard.at(coords);
+      cell.innerHTML = symbol;
+    })
+  }
 })()
